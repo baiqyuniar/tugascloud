d41fc231482b57927de349a0ecaf00419f212699
baiqyustiarini <baiqyustiarini@gmail.com>
2021-10-16 16:09:47 +0700

    initial commit

---
+++ mqttPub.py
@@ -0,0 +1,29 @@
+# import paho.mqtt.client as mqtt
+
+# client = mqtt.Client()  
+
+# client.connect("localhost", 1884, 60)       #parameternya IPAddr, port, timeout
+
+
+# while True:
+#     payload = input("Masukkan angka: ")
+
+#     print("Publishing message . . .")
+#     client.publish("Angka", payload)
+#     print("Message Published\n")
+
+
+import paho.mqtt.client as mqtt 
+from random import randrange, uniform
+import time
+
+mqttBroker ="mqtt.eclipseprojects.io" 
+
+client = mqtt.Client("Temperature_Inside")
+client.connect(mqttBroker) 
+
+while True:
+    randNumber = uniform(20.0, 21.0)
+    client.publish("TEMPERATURE", randNumber)
+    print("Just published " + str(randNumber) + " to topic TEMPERATURE")
+    time.sleep(1)
---
+++ mqttSub.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):            #akan berjalan saat berhasil connect ke broker, yang dijalankan subscribe
+    client.subscribe("Angka")
+
+def on_message(client, userdata, message):              #akan berjalan saat ada pesan yang diterima
+    hasil = message.payload.decode('utf-8')
+    print("Nilai :", hasil)
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("localhost", 1884, 60)
+
+client.loop_forever()                                   #client nunggu pesan masuk
